749f1791e5badb742be1e85e32fa39010fc3d067
Giovanni Palmieri <gianni00palmieri@gmail.com>
2023-03-22 16:16:49 +0100

    login logic

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host     : process.env.HOST,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DB
-});
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB
+})
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 const app = express()
-  .use(cors())
-  .use(bodyParser.json())
+    .use(cors())
+    .use(bodyParser.json())
+
+
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body
+    const sql_query = "SELECT 1 FROM persona WHERE mail = ? AND password = ?"
+
+    connection.query(sql_query, [email, password]).then(([rows, fields]) => {
+        if (rows.length > 0) {
+            res.send({ status: 200, msg: "OK" })
+        } else {
+            res.send({ status: 404, msg: "User not found" })
+        }
+    })
+
+})
+
 
 app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
-  connection.query('SELECT * FROM tipologia_documento').then(([result]) => {
-        console.log(result)
-  });
-});
+    console.log(`Express server listening on port ${port}`)
+})
