a22b65763269f38e3694cf8e404dd259c0a3a738
Giovanni Palmieri <gianni00palmieri@gmail.com>
2023-03-23 09:16:02 +0100

    hold storage for files

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8080
 const app = express()
     .use(cors())
     .use(bodyParser.json())
-
+    .use(express.static('public'))
 
 app.post("/login", async (req, res) => {
     let { email, password } = req.body
